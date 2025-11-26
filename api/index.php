8b97cb0d081b585d5592e3afd4a320d14ce641eb
Adam Rizqi <adamrizqi617@gmail.com>
2025-11-26 15:43:48 +0700

    Fix read-only filesystem error for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Load Composer's autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load The Application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+}
+$app->useStoragePath($storagePath);
+
+// Handle The Request
+$request = Illuminate\Http\Request::capture();
+$response = $app->handle($request);
+$response->send();
+$app->terminate();
